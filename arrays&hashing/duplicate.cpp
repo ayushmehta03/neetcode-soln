229fc48d2dd08d13c0dc750dc7400cd7b7811411
Ayush Mehta <ayushmehtabps@gmail.com>
2025-09-28 14:39:12 +0530

    initial

---
+++ arrays&hashing/duplicate.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+using namespace std;
+
+
+
+
+bool check(vector<int>&nums){
+    // declare unordered map for keeping track of frequencies
+    unordered_map<int,int>mp;
+
+    for(auto num:nums) mp[num]++;
+
+    // if the freq is greater or equal to 2 it means there exists a duplicate
+    for(auto it:mp){
+        if(it.second>=2) return true;
+    }
+    return false;
+
+}
+
+int main(){
+
+    int n;
+    cin>>n;
+    vector<int>nums(n);
+    for(int i=0;i<n;i++) cin>>nums[i];
+
+    if(check(nums)) cout<<"Yes there exists a dupliate";
+    else cout<<"No duplicate in the array";
+    return 0;
+}
