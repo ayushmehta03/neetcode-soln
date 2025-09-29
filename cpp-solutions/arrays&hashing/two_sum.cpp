499d6eee9dbd1f192c91d339606725d5a34e9032
Animesh-Jantwal <animeshjantwalcoding@gmail.com>
2025-09-29 21:23:52 +0530

    Fix: corrected bug related to issue #1

--- cpp-solutions/arrays&hashing/two_sum.cpp
+++ cpp-solutions/arrays&hashing/two_sum.cpp
@@ -4,44 +4,44 @@
 using namespace std;
 
 
-vector<int>twoSum(vector<int>&nums,int target){
-    unordered_map<int,int>mp;
+// Approach -> Hashmap
+// Time Complexity -> O(n)
+// Space Complexity -> O(n) (auxiliary space for hashmap)
 
+vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int, int> mp;
 
-    for(int i=0;i<nums.size();i++){
-        int diff=target-nums[i];
-        if(mp.count(diff)){
-            return {mp[diff],i};
+    for (int i = 0; i < nums.size(); i++) {
+        int diff = target - nums[i];
+        if (mp.count(diff)) {
+            return {mp[diff], i};
         }
-
-        mp[nums[i]]=i;
+        mp[nums[i]] = i;
     }
 
     return {};
-
 }
 
 
-int main(){
+int main() {
     int n;
-    cout<<"Enter the number of elements: ";
-    cin>>n;
-    vector<int>nums(n);
+    cout << "Enter the number of elements: ";
+    cin >> n;
+    vector<int> nums(n);
 
-    cout<<"Enter the array elements: ";
-    for(int i=0;i<n;i++) cin>>nums[i];
+    cout << "Enter the array elements: ";
+    for (int i = 0; i < n; i++) cin >> nums[i];
 
     int target;
-    cout<<"Enter the target: ";
-    cin>>target;
+    cout << "Enter the target: ";
+    cin >> target;
 
-    vector<int>ans=twoSum(nums,target);
+    vector<int> ans = twoSum(nums, target);
 
-    if(ans.empty())  cout<<"no two elements having target sum";
-    else for(auto it:ans) cout<<" "<<it;
+    if (ans.empty())  
+        cout << "No two elements having target sum";
+    else 
+        for (auto it : ans) cout << " " << it;
+
     return 0;
-
-
-
-
 }
