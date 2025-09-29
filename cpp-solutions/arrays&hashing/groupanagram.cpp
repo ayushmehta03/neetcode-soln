b0357c3f7104c90f7e47602ae13c82da66ceb8b4
Ayush Mehta <ayushmehtabps@gmail.com>
2025-09-29 18:43:25 +0530

    comment approach

--- cpp-solutions/arrays&hashing/groupanagram.cpp
+++ cpp-solutions/arrays&hashing/groupanagram.cpp
@@ -8,14 +8,24 @@ using namespace std;
 
 
 vector<vector<string>>groupAnagrams(vector<string>&strs){
+    // 2d vector for stroing the groups which match
     vector<vector<string>>ans;
+    // unordered-map for storing the same type
     unordered_map<string,vector<string>>mp;
     for(auto &str:strs){
         string word=str;
+        // sort the word what this will do is 
+        // suppose we have eat and ate so for both sorted order will be aet
+
         sort(str.begin(),str.end());
+        
+        // now aet will contain ->eat and ate both
+
         mp[str].push_back(word);
 
     }
+
+    // iterate through the map and push it to 2d vector answer
     for(auto it:mp){
         ans.push_back(it.second);
     }
