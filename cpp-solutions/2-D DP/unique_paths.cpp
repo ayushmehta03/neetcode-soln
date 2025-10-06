d28a95b93a5d3d403eedb5c9d88e3317da6345bb
Ayush Mehta <ayushmehtabps@gmail.com>
2025-10-06 15:47:08 +0530

    unique-ways 2d dp solution  tabulation

--- cpp-solutions/2-D DP/unique_paths.cpp
+++ cpp-solutions/2-D DP/unique_paths.cpp
@@ -8,9 +8,25 @@ using namespace std;
 
 
 
-int uniqueWays(){
+int uniqueWays(int n,int m){
+    // declare dp vector nxm
+    vector<vector<int>>dp(n,vector<int>(m,0));
 
-}
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(i==0&&j==0) dp[i][j]=1;
+            else{
+                int up=0,left=0;
+                if(i>0)  up=dp[i-1][j];
+                if(j>0)  left=dp[i][j-1];
+                dp[i][j]=up+left;
+            }
+
+        }
+    }
+    return dp[n-1][m-1];
+
+}   
 
 
 
@@ -64,8 +80,14 @@ int main(){
 
 
     // memoization technique 
+    /*
     int ans=f(n-1,m-1,dp);
-    cout<<"The total no of ways through which we can reach the end point: "<<ans;
+    cout<<"The total no of ways through which we can reach the end point: "<<ans; */
     
+
+
+    int uniquePaths=uniqueWays(n,m);
+    cout<<uniquePaths;
+
 
 }
