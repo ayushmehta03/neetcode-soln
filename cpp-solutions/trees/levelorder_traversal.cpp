ca785d256b1fa82f54754346179e49c2fbf54e2f
Ayush Mehta <ayushmehtabps@gmail.com>
2025-10-14 21:49:06 +0530

    add level_order_traversal bst

--- cpp-solutions/trees/levelorder_traversal.cpp
+++ cpp-solutions/trees/levelorder_traversal.cpp
@@ -3,6 +3,8 @@
 #include<queue>
 using namespace std;
 
+// constructing the binary search tree 
+
 struct Node{
 int data;
 Node*left;
@@ -21,16 +23,80 @@ Node*insert(Node*root,int val){
     }
     else{
         root->right=insert(root->right,val);
-        return root;
     }
+    return root;
 }
 
 
+
+
+vector<vector<int>>levelOrder(Node*root){
+
+    // declaring 2d vector for returning answer
+    vector<vector<int>>ans;
+
+    // return empty vector if root is null
+    if(root==NULL) return ans;
+
+    // declare a queue of same type as struct so that it holds value left and right
+    queue<Node*>q;
+
+    // push the root at first
+    q.push(root);
+
+    // run untill the q becomes empty
+
+    while(!q.empty()){
+
+    // grab the size of queue
+     int size=q.size();
+
+     // declare vector so that it stores value of each value
+     vector<int>level;
+
+    // run the operation fwrt to the size
+     for(int i=0;i<size;i++){
+     Node*node=q.front();
+
+     // pop the value from the top
+     q.pop();
+
+     // check for both left and right if it is not null push to the queue theses are the other levels
+     if(node->left!=NULL) q.push(node->left);
+     if(node->right!=NULL) q.push(node->right);
+
+     // push the value to the leevel vector
+     level.push_back(node->data);
+
+     }
+     // once the loop is completed for each level push it to the 2d vector ans 
+
+     ans.push_back(level);
+
+    }
+
+    return ans;
+
+
+}
+
 int main(){
     Node*root=NULL;
-    vector<int>values={3,9,20,NULL,NULL,15,7};
+    vector<int>values={3,9,20,15,7};
 
  for(int val:values){
     root=insert(root,val);
  }
+
+ vector<vector<int>>ans=levelOrder(root);
+ 
+ for(int i=0;i<ans.size();i++){
+    for(auto it: ans[i]){
+     cout << it << " ";
+    }
+    cout<<endl;
+ }
+
+
+
 }
