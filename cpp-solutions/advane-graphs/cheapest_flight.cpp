ce928c233b4548fa43a4a4ca6d9a7b47cd49f29a
Ayush Mehta <ayushmehtabps@gmail.com>
2025-10-20 13:20:59 +0530

    cheapest_flight advance graph

--- cpp-solutions/advane-graphs/cheapest_flight.cpp
+++ cpp-solutions/advane-graphs/cheapest_flight.cpp
@@ -23,32 +23,45 @@ int cheapestFlight(int n,vector<vector<int>>&flights,int src,int dst,int k){
     // distance vector for stroing the shortest distance
         vector<int>distance(n,1e9);
 
-        // pushing the source to the queue
+        // pushing the source to the queue with stops as 0
     q.push({0,{src,dst}});
 
     // source distance should be 0
     distance[src]=0;
 
     while(!q.empty()){
-                    auto it = q.front();
+
+        // grab the queue element and pop it right after it
+          auto it = q.front();
             q.pop();
 
+            // take all 3 params stops node and cost 
             int stops = it.first;
             int node = it.second.first;
             int cost = it.second.second;
 
+            // stops greater than k we will continue 
             if (stops > k) continue;
 
+
+            // traverse inside the vector adjacent
             for (auto &itr : adj[node]) {
+
+                // take the adj node and weight 
                 int adjNode = itr.first;
                 int wt = itr.second;
-
+                // if we get a smaller distance then will replace it
                 if (cost + wt < distance[adjNode]) {
+
+                    // replace the current distance
                     distance[adjNode] = cost + wt;
+                    // push the elemnt into queue
                     q.push({stops + 1, {adjNode, cost + wt}});
                 }
             }
     }
+
+    // if the last dst in distance vector value is 1e9 will return -1 which is not possible
     return (distance[dst] == (int)1e9) ? -1 : distance[dst];
 
 
