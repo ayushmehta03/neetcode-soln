d2504499171987eec3aaf868f06d0f64f1586c5a
Ayush Mehta <ayushmehtabps@gmail.com>
2025-10-23 11:41:07 +0530

    two sum2 input is sorted

--- cpp-solutions/two-pointers/twosum||.cpp
+++ cpp-solutions/two-pointers/twosum||.cpp
@@ -23,10 +23,15 @@ vector<int> twoSum(vector<int>&nums,int target){
         // if it is equal to the target we need to return the index but in the guven problem the index is starting from 1 so will return +1 WITH the matching pointers
         if(sum==target) return {left+1,right+1};
 
+        // if target is greater will move in left to right cause its increasing
         else if(sum<target) left++;
 
+
+        // and if sum is greater than taregt will move right to left whuch us decreasing order
         else right--;
  }
+
+ // no target sum found return -1 -1
 
     return {-1,-1};
 
@@ -34,16 +39,22 @@ vector<int> twoSum(vector<int>&nums,int target){
 
 int main(){
 
+
+    // declaring the required input data
     vector<int>nums={2,7,11,15};
     int target=9;
 
-
+    // store it into ans vector
     vector<int>ans=twoSum(nums,target);
 
+
+    // if both are -1 will return not possible
 
     if(ans[0]==-1 &&ans[1]==-1){
         cout<<"no elents can make the sum to the target";
     }
+
+    // just log the data
     else{
 
     cout<<"The matching indexes which makes target sum are : ";
